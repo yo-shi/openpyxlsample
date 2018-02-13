6fd044aca4a00ba07ca2b710365cec37de5a047b
yo-shi <yskshi007@gmail.com>
2018-02-13 21:17:54 +0900

    change output folder name.

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 This repository is collection of snipets for openpyxl.
 
 ## TODO
-- [ ] Open new excel file.
+- [x] Open new excel file.
 - [ ] Open exist excel file.
 - [ ] Get Sheet Object.
 - [ ] Write cells.
-- [ ] Save to new excel file.
+- [x] Save to new excel file.
--- src/create_new_file.py
+++ src/create_new_file.py
@@ -2,11 +2,11 @@
 import openpyxl as px
 import os
 
-if not os.path.exists('debug'):
-    os.mkdir('debug')
+if not os.path.exists('output'):
+    os.mkdir('output')
 
 # Create new Workbook object.
 wb = px.Workbook()
 
 # Save Workbook with name.
-wb.save('./debug/new_file.xlsx')
+wb.save('./output/new_file.xlsx')
